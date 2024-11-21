7509632242677d6e6dfb816b906c44c66fa50190
Stanislav Melnikov <stanislav.melnikov@gmail.com>
2024-11-21 18:13:29 +0100

    new pke tests

--- pke/test_pke.py
+++ pke/test_pke.py
@@ -9,6 +9,12 @@ import json
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from common.test_tools import Tools, ImageDir
 
+def print_channel_info(channel_index, channel):
+    print(f"Channel {channel_index} info:")
+    print(f"\tMinimum: {np.min(channel)}")
+    print(f"\tMaximum: {np.max(channel)}")
+    print(f"\tAverage: {np.mean(channel)}")
+
 class TestVsi(unittest.TestCase):
 
     def test_multichannel_slide(self):
@@ -33,6 +39,45 @@ class TestVsi(unittest.TestCase):
             print("last__slice: ", np.max(last__slice))   
             self.assertFalse(np.array_equal(first_slice, last__slice))
 
+
+    def test_multichannel_slide_no_scale_separate_channels(self):
+        file_path = Tools().getImageFilePath("pke", "openmicroscopy/PKI_scans/LuCa-7color_Scan1.qptiff", ImageDir.FULL)
+        file_name = "LuCa-7color_Scan1.qptiff - resolution #1 (1, x=11619, y=16875, w=1202, h=756).tif"
+        ref_image_path = Tools().getTestImagePath("pke", file_name)
+        ref_image = Image.open(ref_image_path)
+        region_rect = (11619, 16875, 1202, 756)
+        with slideio.open_slide(file_path, "QPTIFF") as slide:
+            self.assertIsNotNone(slide)
+            num_scenes = slide.num_scenes
+            self.assertEqual(num_scenes, 1)
+            with slide.get_scene(0) as scene:
+                num_channels = scene.num_channels
+                self.assertEqual(num_channels, 5)
+                for channel_index in range(num_channels):
+                    ref_image.seek(channel_index)
+                    channel_raster = scene.read_block(region_rect, channel_indices=[channel_index])
+                    ref_raster = np.array(ref_image)
+                    self.assertTrue(np.array_equal(channel_raster, ref_raster))
+                    
+    def test_multichannel_slide_no_scale_all_channels(self):
+        file_path = Tools().getImageFilePath("pke", "openmicroscopy/PKI_scans/LuCa-7color_Scan1.qptiff", ImageDir.FULL)
+        file_name = "LuCa-7color_Scan1.qptiff - resolution #1 (1, x=11619, y=16875, w=1202, h=756).tif"
+        ref_image_path = Tools().getTestImagePath("pke", file_name)
+        ref_image = Image.open(ref_image_path)
+        region_rect = (11619, 16875, 1202, 756)
+        with slideio.open_slide(file_path, "QPTIFF") as slide:
+            self.assertIsNotNone(slide)
+            num_scenes = slide.num_scenes
+            self.assertEqual(num_scenes, 1)
+            with slide.get_scene(0) as scene:
+                num_channels = scene.num_channels
+                self.assertEqual(num_channels, 5)
+                image_raster = scene.read_block(region_rect)
+                for channel_index in range(num_channels):
+                    ref_image.seek(channel_index)
+                    ref_raster = np.array(ref_image)
+                    channel_raster = image_raster[:, :, channel_index]
+                    self.assertTrue(np.array_equal(channel_raster, ref_raster))
 
         
 if __name__ == '__main__':
